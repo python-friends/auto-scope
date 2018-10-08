c92f1cf1a9c0c4d8183043f00886f10c949a1b9e
Wytamma Wirth <wytamma.wirth@me.com>
2018-10-09 00:16:02 +1000

    added logging util

---
+++ autoscope/utils.py
@@ -0,0 +1,20 @@
+import logging
+
+def _set_up_logging():# create logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    return logger.addHandler(ch)
+
+log = _set_up_logging()
