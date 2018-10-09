2164fce510807f83d5b783e9e2f174630a9ff48a
Wytamma Wirth <wytamma.wirth@me.com>
2018-10-09 21:09:48 +1000

    added test for focus

--- tests/test_autoscope.py
+++ tests/test_autoscope.py
@@ -3,8 +3,15 @@ from autoscope import Autoscope
 
 class TestAutoscope():
     def setup(self):
-        self.scope = Autoscope([1,2,3,4],[5,6,7,8])
+        self.scope = Autoscope([1,2,3,4],[5,6,7,8],[9,10,11,12])
 
     def test_stage_attr(self):
         if not hasattr(self.scope, "stage"):
             pytest.fail("Auto scope does not have attribute 'stage'")
+
+    def test_focus_attr(self):
+        if not hasattr(self.scope, "focus"):
+            pytest.fail("Auto scope does not have attribute 'focus'")
+
+    def test_coordinates(self):
+        assert self.scope.coordinates == (0,0,0)
