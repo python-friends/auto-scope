d050bb8049212a820d5d3de514a6ae60b94159f1
Wytamma Wirth <wytamma.wirth@me.com>
2018-10-09 20:57:08 +1000

    zstep

--- autoscope/focus.py
+++ autoscope/focus.py
@@ -6,22 +6,25 @@ from .core import step
 
 class Focus:
 
-    def __init__(self, pins):
-        self.pins = pins
-        self.postion = 0
+    def __init__(self, zpins):
+        self.zpins = zpins
+        self.zpos = 0
 
     def set_home(self):
-        self.postion = 0
+        self.zpos = 0
+
+    def step_z(self, nsteps):
+        self.zpos += step(self.zpins, nsteps)
     
     def up(self, nsteps):
-        step(self.pins, nsteps=abs(nsteps))
+        self.step_z(nsteps=abs(nsteps))
 
     def down(self, nsteps):
-        step(self.pins, nsteps=(-1 * abs(nsteps)))
+        self.step_z(nsteps=(-1 * abs(nsteps)))
 
     def goto(self, postion):
         nsteps = postion - self.postion
-        step(self.pins, nsteps) 
+        self.step_z(nsteps) 
         
     def home(self):
         self.goto(0)
