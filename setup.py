e7681552e672f75fd0822a6d9952f97848aeda66
Wytamma Wirth <wytamma.wirth@me.com>
2018-10-09 11:19:38 +1000

    removed Rpi.GPIO install

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Wytamma Wirth & Eike Steinig',
     author_email='python.friends.py@gmail.com',
     packages=find_packages(),
-    install_requires=['RPi.GPIO'],
+    install_requires=[],
     version='0.1',
     license='MIT',
     description='An open source whole slide scanner and automated cell discrimination and counting system'
