0eb05b1c32328ba30f3ad5216a72223bb867eb9e
Eike Steinig <jc225327@ad.jcu.edu.au>
2018-10-15 18:48:37 +1000

    add uuid, leaflet update

--- scopy/static/js/scopy.js
+++ scopy/static/js/scopy.js
@@ -3,20 +3,25 @@ function makeSlideMap() {
 
     // Get the original image for setting the
     // width and height for Raster Coordinates:
-
     var img = new Image();
+
+    // This will hold the marker data
+    // on click and remove, for saving to DB:
+    var markerData = {};
 
     // Asynchronous loading of image, wait for it
     // to be loaded first, then use the dimensions
     // to generate the raster coordinate tiles:
-
     img.onload = function(){
 
         var minZoom = 1;
         var maxZoom = 4;
 
         // create the map
-        var map = L.map('llmap');
+        var map = L.map('llmap', {
+            minZoom: minZoom,
+            maxZoom: maxZoom
+        });
 
         var dim = [img.height, img.width];
 
@@ -25,48 +30,44 @@ function makeSlideMap() {
 
         console.log(dim);
 
-        // all coordinates need to be unprojected using the `unproject` method
-        // set the view in the lower right edge of the image
+        // All coordinates need to be un-projected using the `unproject` method:
         map.setView(rc.unproject([dim[0]/2, dim[1]/2]), 3);
 
-        // set markers on click events in the map
+        // Set markers on click events in the map:
         map.on('click', function (event) {
-          // any position in leaflet needs to be projected to obtain the image coordinates
-          var coords = rc.project(event.latlng);
-          var marker = L.marker(rc.unproject(coords))
-            .addTo(map);
+            // Any position in leaflet needs to be projected
+            // to obtain the image coordinates:
+            var coords = rc.project(event.latlng);
+            var markerID = uuid();
+            var marker = L.marker(rc.unproject(coords), {'markerID': markerID}).addTo(map);
 
-          marker.on('click', function(){
+            markerData[markerID] = marker;
 
-              marker.bindPopup('[' + Math.floor(coords.x) + ',' + Math.floor(coords.y) + ']')
-                .openPopup()
-              // marker.remove()
+            console.log("Pushed to", markerData);
+            // Remove markers:
+            marker.on('click', function(){
+                delete markerData[markerID];
+                marker.remove();
+                console.log("Popped", markerData);
           })
         });
 
-        var southWest = rc.unproject([dim[0], 0]);
-        var northEast = rc.unproject([0, dim[1]]);
+        // Maximum bounds for slide tiles:
+        var bottomLeft = rc.unproject([dim[0], 0]);
+        var topRight = rc.unproject([0, dim[1]]);
+        var bounds = new L.LatLngBounds(bottomLeft, topRight);
 
-        var bounds = new L.LatLngBounds(southWest, northEast);
-
-        // var bounds = L.latLngBounds(southWest, northEast);
-
-        console.log(southWest, northEast, bounds);
-
-        map.setMaxBounds(bounds);
-
-        // the tile layer containing the image generated with `gdal2tiles --leaflet -p raster -w none <img> tiles`
+        // Tile layer containing the image generated with
+        // `gdal2tiles --leaflet -p raster -w none <img> tiles`
         L.tileLayer('/static/assets/tiles/{z}/{x}/{y}.png', {
-            minZoom: minZoom,
-            maxZoom: maxZoom,
-            noWrap: true
+            noWrap: true,
+            bounds: bounds
         }).addTo(map);
 
     };
 
     // Pour in the image:
     img.src = '/static/assets/Image_4923.jpg';
-
 
 }
 
